3d7d3f57278f3c0b8297577d8027f7af8582d7cc
Neil <mallarineil9@gmail.com>
2025-02-27 12:57:53 +0800

    Render Note labels on Drawer

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -6,15 +6,22 @@ import { logoutUser } from '@/services/user';
 import { User } from 'firebase/auth';
 import { getNotes } from '@/services/note';
 import { useQuery } from '@tanstack/react-query';
-interface StaticItem {
-  text: 'Notes' | 'Log out';
-  path: '/notes' | '/home';
+import kebabCase from 'just-kebab-case';
+
+interface DrawerItem {
+  text: string;
+  path: string;
   icon: JSX.Element;
+  isLabel?: boolean;
 }
 
-const staticItem: StaticItem[] = [
+const staticItems: DrawerItem[] = [
   { text: 'Notes', path: '/notes', icon: <Icons.Note size={24} /> },
-  { text: 'Log out', path: '/home', icon: <Icons.Logout size={24} /> },
+  {
+    text: 'Log out',
+    path: '/home',
+    icon: <Icons.Logout size={24} />,
+  },
 ];
 
 interface DrawerProps {
@@ -34,6 +41,27 @@ const Drawer = ({ user }: DrawerProps) => {
   });
 
   if (!notes) return;
+
+  // This line: (1) Flattens an Array of Objects of Array 'labels',
+  // (2) turn it into a Set (to filter duplicate values), then
+  // (3) converts it back to Array
+  const labels = Array.from(new Set(notes.flatMap((note) => note.labels)));
+
+  const labelItems: DrawerItem[] = labels.map((label) => {
+    return {
+      text: label,
+      path: `/labels/${kebabCase(label)}`,
+      icon: <Icons.Label size={24} />,
+      isLabel: true,
+    };
+  });
+
+  const drawerItems = [
+    ...staticItems.slice(0, -1),
+    ...labelItems,
+    // Log out item
+    ...staticItems.slice(-1),
+  ];
 
   return (
     <div className='flex'>
@@ -59,7 +87,7 @@ const Drawer = ({ user }: DrawerProps) => {
             className='drawer-overlay'
           ></label>
           <ul className='menu bg-base-200 min-h-full w-80 py-8 px-0'>
-            {staticItem.map((item) => (
+            {drawerItems.map((item) => (
               <li
                 // If item is Log out, push item at the bottom of the drawer
                 className={classNames({ 'mt-auto': item.text === 'Log out' })}
@@ -70,11 +98,18 @@ const Drawer = ({ user }: DrawerProps) => {
                   className={({ isActive }) =>
                     classNames('gap-5 font-medium px-6 py-3 rounded-none', {
                       'bg-primary/70': isActive,
+                      group: item.isLabel,
                     })
                   }
                   onClick={item.text === 'Log out' ? logoutUser : undefined}
                 >
                   {item.icon} <span>{item.text}</span>
+                  <button
+                    data-label={item.text}
+                    className='btn btn-ghost invisible group-hover:visible'
+                  >
+                    Edit
+                  </button>
                 </NavLink>
               </li>
             ))}
