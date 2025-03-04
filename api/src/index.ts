ad4205f07ff596df23b474f8546bfd1bb8dac3b5
Neil <mallarineil9@gmail.com>
2025-03-04 22:35:49 +0800

    Install cors package

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,9 +2,11 @@ import express from 'express';
 import userRouter from '@routes/user';
 import noteRouter from '@routes/note';
 import { errorHandler } from '@util/middleware';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/notes', noteRouter);
